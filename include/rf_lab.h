2f77d6114f539f26be4e079b6bb78e979fe7ffc2
merberg <j1mb3rg@gmail.com>
2026-09-06 21:45:54 -0700

    debug: expand RF Lab receive telemetry

--- include/rf_lab.h
+++ include/rf_lab.h
@@ -6,10 +6,16 @@ namespace ywd {
 
 struct RfStats {
   uint32_t txCount = 0;
+  uint32_t rawRxCount = 0;
   uint32_t rxCount = 0;
   uint32_t crcOrRxErrors = 0;
   uint32_t duplicateCount = 0;
   uint32_t missedSequenceEstimate = 0;
+  uint32_t selfPacketCount = 0;
+  uint32_t foreignPacketCount = 0;
+  uint32_t rxArmCount = 0;
+  uint32_t rxArmErrors = 0;
+  uint32_t dio1PollHits = 0;
   uint32_t lastRxSequence = 0;
   bool haveLastRxSequence = false;
   float lastRssi = 0.0f;
